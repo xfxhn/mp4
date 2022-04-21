8729195bbb92c7aea63aefd775b80423a23c760e
xiaofeng <m17620165420@163.com>
2022-04-21 18:10:27 +0800

    decode AVC configureation

--- src/visualSampleEntry/visualSampleEntry.h
+++ src/visualSampleEntry/visualSampleEntry.h
@@ -13,10 +13,27 @@ private:
     uint8_t profile_compatibility;
     uint8_t AVCLevelIndication;
     uint8_t lengthSizeMinusOne;
-    uint8_t numOfSequenceParameterSets;
 
+    uint8_t numOfSequenceParameterSets;
     /*seq_parameter_set_id在0-31 的范围内，包括0和31*/
-    uint8_t *sequenceParameterSets[32];
+    uint16_t sequenceParameterSetLength[32]{0};
+    uint8_t *sequenceParameterSetNALUnit[32]{nullptr};
+
+    uint8_t numOfPictureParameterSets;
+    /*pic_parameter_set_id的值应该0到255的范围内（包括0 和255）。*/
+    uint16_t pictureParameterSetLength[256]{0};
+    uint8_t *pictureParameterSetNALUnit[256]{nullptr};
+
+
+    uint8_t chroma_format{1};
+    uint8_t bit_depth_luma_minus8{0};
+    uint8_t bit_depth_chroma_minus8{0};
+
+
+    uint8_t numOfSequenceParameterSetExt{0};
+    uint16_t sequenceParameterSetExtLength[256]{0};
+    uint8_t *sequenceParameterSetExtNALUnit[256]{nullptr};
+
 public:
     /*防止隐式调用构造函数*/
     explicit AVCDecoderConfigurationRecord(BitStream &bs);
